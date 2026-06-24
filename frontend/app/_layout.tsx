dc8ed4b8b6d0f36785de942d1801fd8f6ac6f4c9
Ainoa Dura Bordonado <ainoadura@outlook.com>
2026-06-24 12:18:46 +0200

    Delete comented lines

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -1,38 +1,4 @@
 // app/_layout.tsx
-/*import { Stack } from 'expo-router';
-import { GluestackUIProvider } from '../components/ui/gluestack-ui-provider';
-import { useTheme } from '../src/constants/theme';
-import { StatusBar } from 'expo-status-bar';
-import '../global.css'; 
-
-
-export default function RootLayout() {
-  const { colors } = useTheme();
-
-  return (
-    <GluestackUIProvider mode="light">
-      <StatusBar style="light" />
-      <Stack
-        screenOptions={{
-          headerStyle: {
-            backgroundColor: colors.surface || '#1C1C1E',
-          },
-          headerTintColor: colors.text || '#FFFFFF',
-          headerTitleStyle: {
-            fontWeight: 'bold',
-          },
-        }}
-      >
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="note/[id]" options={{ title: 'Media Detail' }} />
-        <Stack.Screen name="new-note" options={{ title: 'New Note', presentation: 'modal' }} />
-        
-      </Stack>
-    </GluestackUIProvider>
-  );
-}
-*/
-// app/_layout.tsx
 import { useEffect } from 'react'; // 1. Importamos useEffect de React
 import { Stack } from 'expo-router';
 import { GluestackUIProvider } from '../components/ui/gluestack-ui-provider';
